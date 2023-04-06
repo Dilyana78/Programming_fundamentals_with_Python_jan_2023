f0ac22f5318614308d8fdb9e01bfe13868a6cdc9
Dilyana78 <122824954+Dilyana78@users.noreply.github.com>
2023-04-06 19:12:56 +0300

    Made order of problems more perspicuous

--- 3_lists_basics_lab/05_numbers_filter.py
+++ 3_lists_basics_lab/05_numbers_filter.py
@@ -24,3 +24,24 @@ for number in numbers:
     if filtered_passed:
         filtered_numbers.append(number)
 print(filtered_numbers)
+
+# 2 version
+# number_of_lines = int(input())
+# list_of_numbers = []
+#
+# for line in range(number_of_lines):
+#     current_number = int(input())
+#     list_of_numbers.append(current_number)
+#
+# command = input()
+# filtered_list = []
+# for num in list_of_numbers:
+#     if command == "positive" and num >= 0:
+#         filtered_list.append(num)
+#     elif command == "negative" and num < 0:
+#         filtered_list.append(num)
+#     if command == "even" and num % 2 == 0:
+#         filtered_list.append(num)
+#     if command == "odd" and num % 2 != 0:
+#         filtered_list.append(num)
+# print(filtered_list)
